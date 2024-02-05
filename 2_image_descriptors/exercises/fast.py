c92233d945d4539421db1bf23cae7e83dc4a71f5
joelsnz <joel04slm@gmail.com>
2024-02-05 19:04:02 +0100

    Add function placeholders

---
+++ 2_image_descriptors/exercises/fast.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def get_bresenham_circle(radius_px):
+    """ 
+    Computes the Bresenham's line algorithm to find the circumference
+    pixels of a point.
+    :param radius_px: radius of the circle in pixels.
+    :return: clockwise ordered list with the circumference pixels.
+    """
+    pass
+
+
+def get_fast_points(im, n_contiguous_pixels, threshold):
+    """
+    :return: Corners detected from FAST Corner Detector.
+    """
+    pass
+
+
+def high_speed_test(fast_points):
+    """
+    Test for rejecting non-corner FAST points.
+    :return: List with filtered FAST points.
+    """
+    pass
