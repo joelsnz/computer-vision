74fe54803c7de3159491332c0b6bd432e5f4ca52
joelsnz <joel04slm@gmail.com>
2024-03-07 13:34:55 +0100

    Turning torso

--- 3_image_to_image_mappings/warp.py
+++ 3_image_to_image_mappings/warp.py
@@ -4,27 +4,28 @@ from numpy import linalg
 import homography
 from scipy import ndimage
 from scipy.spatial import Delaunay
+import matplotlib.pyplot as plt
 
 
 def alpha_for_triangle(points, m, n):
-    """ Creates alpha map of size (m, n)
-        for a triangle with corners defined by points
-        (given in normalized homogeneous coordinates). """
+    """Creates alpha map of size (m, n)
+    for a triangle with corners defined by points
+    (given in normalized homogeneous coordinates)."""
 
     alpha = np.zeros((m, n))
     for i in range(min(points[0]), max(points[0])):
         for j in range(min(points[1]), max(points[1])):
             x = linalg.solve(points, [i, j, 1])
-            if min(x) > 0: # all coefficients positive
+            if min(x) > 0:  # all coefficients positive
                 alpha[i, j] = 1
 
     return alpha
 
 
 def image_in_image(im1, im2, tp):
-    """ Put im1 in im2 with an affine transformation
-        such that corners are as close to tp as possible.
-        tp are homogeneous and counter-clockwise from top left. """
+    """Put im1 in im2 with an affine transformation
+    such that corners are as close to tp as possible.
+    tp are homogeneous and counter-clockwise from top left."""
 
     # points to warp from
     m, n = im1.shape[:2]
@@ -32,17 +33,16 @@ def image_in_image(im1, im2, tp):
 
     # compute affine transformation and apply
     H = homography.Haffine_from_points(tp, fp)
-    im1_t = ndimage.affine_transform(im1, H[:2, :2],
-                                     (H[0, 2], H[1, 2]), im2.shape[:2])
+    im1_t = ndimage.affine_transform(im1, H[:2, :2], (H[0, 2], H[1, 2]), im2.shape[:2])
     alpha = im1_t > 0
 
     return (1 - alpha) * im2 + (alpha * im1_t)
 
 
 def advanced_image_in_image(im1, im2, tp):
-    """ Put im1 in im2 with two affine transformations
-        to ensure all corners are on tp.
-        tp are homogeneous and counter-clockwise from top left. """
+    """Put im1 in im2 with two affine transformations
+    to ensure all corners are on tp.
+    tp are homogeneous and counter-clockwise from top left."""
 
     # set from points to corners of im1
     m, n = im1.shape[:2]
@@ -54,8 +54,7 @@ def advanced_image_in_image(im1, im2, tp):
 
     # compute H
     H = homography.Haffine_from_points(tp2, fp2)
-    im1_t = ndimage.affine_transform(im1, H[:2, :2],
-                                     (H[0, 2], H[1, 2]), im2.shape[:2])
+    im1_t = ndimage.affine_transform(im1, H[:2, :2], (H[0, 2], H[1, 2]), im2.shape[:2])
 
     # alpha for triangle
     alpha = alpha_for_triangle(tp2, im2.shape[0], im2.shape[1])
@@ -67,8 +66,7 @@ def advanced_image_in_image(im1, im2, tp):
 
     # compute H
     H = homography.Haffine_from_points(tp2, fp2)
-    im1_t = ndimage.affine_transform(im1, H[:2, :2],
-                                     (H[0, 2], H[1, 2]), im2.shape[:2])
+    im1_t = ndimage.affine_transform(im1, H[:2, :2], (H[0, 2], H[1, 2]), im2.shape[:2])
 
     # alpha for triangle
     alpha = alpha_for_triangle(tp2, im2.shape[0], im2.shape[1])
@@ -77,9 +75,9 @@ def advanced_image_in_image(im1, im2, tp):
 
 
 def triangulate_points(x, y):
-    """ Dealunay triangulation of 2D points. """
+    """Dealunay triangulation of 2D points."""
     if len(x) != len(y):
-        RaiseError(f'x points number {len(x)} does not equal y points number {len(y)}')
+        RaiseError(f"x points number {len(x)} does not equal y points number {len(y)}")
 
     points = [[x[point], y[point]] for point in range(len(x))]
 
@@ -87,12 +85,12 @@ def triangulate_points(x, y):
 
 
 def pw_affine(fromim, toim, fp, tp, tri):
-    """ Warp triangular patches from an image.
-        :param fromim: image to warp
-        :param toim: destination image
-        :param fp: from points in hom. coordiantes
-        :param tp: to points in hom. coordinates
-        :param tri: triangulation. """
+    """Warp triangular patches from an image.
+    :param fromim: image to warp
+    :param toim: destination image
+    :param fp: from points in hom. coordiantes
+    :param tp: to points in hom. coordinates
+    :param tri: triangulation."""
 
     im = toim.copy()
 
@@ -100,7 +98,7 @@ def pw_affine(fromim, toim, fp, tp, tri):
     is_color = len(fromim.shape) == 3
 
     # create image to warp to (needed if iterate colors)
-    im_t = np.zeros(im.shape, 'uint8')
+    im_t = np.zeros(im.shape, "uint8")
 
     for t in tri.simplices:
         # compute affine transformation
@@ -108,14 +106,13 @@ def pw_affine(fromim, toim, fp, tp, tri):
 
     if is_color:
         for col in range(fromim.shape[2]):
-            im_t[:, :, col] = ndimage.affine_transform(fromim[:, :, col], 
-                                                       H[:2, :2],
-                                                       (H[0, 2], H[1, 2]),
-                                                       im.shape[:2])
+            im_t[:, :, col] = ndimage.affine_transform(
+                fromim[:, :, col], H[:2, :2], (H[0, 2], H[1, 2]), im.shape[:2]
+            )
     else:
-        im_t = ndimage.affine_transform(fromim, H[:2, :2],
-                                        (H[0, 2], H[1, 2]),
-                                        im.shape[:2])
+        im_t = ndimage.affine_transform(
+            fromim, H[:2, :2], (H[0, 2], H[1, 2]), im.shape[:2]
+        )
 
     # alpha for triangle
     alpha = alpha_for_triangle(tp[:, t], im.shape[0], im.shape[1])
@@ -127,9 +124,9 @@ def pw_affine(fromim, toim, fp, tp, tri):
 
 
 def plot_mesh(x, y, tri) -> None:
-    """ Plot triangles. """
+    """Plot triangles."""
 
     plt.triplot(x, y, tri.simplices)
-    plt.plot(x, y, '*')
+    plt.plot(x, y, "*")
 
     return
